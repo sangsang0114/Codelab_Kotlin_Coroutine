df82753721e43f0e84f1f14154e5057641461a42
yoon <envy15@skuniv.ac.kr>
2024-02-21 15:33:53 +0900

    Coroutine Dispatcher Example

--- src/Main.kt
+++ src/Main.kt
@@ -3,45 +3,16 @@ import kotlinx.coroutines.*
 
 fun main() {
     runBlocking {
-        println("Weather forecast")
-        println(getWeatherReport())
-        println("Have a nice day!")
-    }
-}
-
-suspend fun getForecast(): String {
-    delay(1000)
-    return "Sunny"
-}
-
-suspend fun getTemperature(): String {
-    delay(500)
-    throw AssertionError("No city Selected")
-    return "30\u00b0C"
-}
-
-suspend fun getWeatherReport() = coroutineScope {
-    val forecast = async { getForecast() }
-    val temperature = async { getTemperature() }
-    delay(200)
-    temperature.cancel()
-    "${forecast.await()}"
-}
-
-/*
-fun mainUsingLaunch() {
-        runBlocking {
-            println("Wheater Forecast")
-            val forecast: Deferred<String> = async {
-                getForecast()
+        println("${Thread.currentThread().name} - runBlocking function")
+        launch {
+            println("${Thread.currentThread().name} - launch function")
+            withContext(Dispatchers.Default) {
+                println("${Thread.currentThread().name} - withContext function")
+                delay(1000)
+                println("10 results found")
             }
-            val temperature: Deferred<String> = async {
-                getTemperature()
-            }
-            println("${forecast.await()} ${temperature.await()}")
-            println("Have a nice day")
+            println("${Thread.currentThread().name} - end of launch")
         }
+        println("Loading...")
     }
-    println(time.toDouble() / 1000)
-    }
- */
+}
